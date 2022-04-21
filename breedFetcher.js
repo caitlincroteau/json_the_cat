5fe2d5a2130b0af40b8d9fa382a5af0cc5b9cd9f
Caitlin <cjcroteau@gmail.com>
2022-04-21 01:07:46 +0000

    add error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,27 @@
 const request = require('request');
 
 const breed = process.argv[2];
-const url = "https://api.thecatapiii.com/v1/breeds/search?q=" + breed;
+const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
 
 
 request(url, (error, response, body) => {
   
   if (error) {
-    console.log("error: ", error);
-    console.log("statusCode: ", response && response.statusCode);
+    console.log("Request error: ", error);
+    return;
+  }
+  if (response && response.statusCode !== 200) {
+    console.log("ERROR MESSAGE: STATUS CODE NOT 200");
+    return;
   }
 
   const data = JSON.parse(body);
-
   if (data.length === 0) {
-    console.log("Breed not found. Please enter another breed.")
-  } 
-  else {
+    console.log("Breed not found. Please enter another breed.");
+  } else {
     console.log(data[0]["description"]);
   }
-}); 
+});
+
+// console.log("response: ", response && response.statusCode);
+// shortcut of if/else statment .... && ...
