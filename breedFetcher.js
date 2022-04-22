5bb6c6b150d3998e831f841bd581a1eceb447614
Caitlin <cjcroteau@gmail.com>
2022-04-22 23:06:30 +0000

    refactor into 2 files. add index and breedFetcherTest

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,54 @@
 const request = require('request');
 
-const breed = process.argv[2];
-const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
+//refactor into a function:
+const fetchBreedDescription = function(breedName, callback) {
+  const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback("Breed not found. Please enter another breed.");
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+
+};
 
 
-request(url, (error, response, body) => {
-  
-  if (error) {
-    console.log("Request error: ", error);
-    return;
-  }
-  if (response && response.statusCode !== 200) {
-    console.log("ERROR MESSAGE: STATUS CODE NOT 200");
-    return;
-  }
+module.exports = { fetchBreedDescription };
 
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Breed not found. Please enter another breed.");
-  } else {
-    console.log(data[0]["description"]);
-  }
-});
+
+//BELOW WORKS:
+
+// const breed = process.argv[2];
+// const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
+
+
+
+// request(url, (error, response, body) => {
+
+//   if (error) {
+//     console.log("Request error: ", error);
+//     return;
+//   }
+//   if (response && response.statusCode !== 200) {
+//     console.log("ERROR MESSAGE: STATUS CODE NOT 200");
+//     return;
+//   }
+
+//   const data = JSON.parse(body);
+//   if (data.length === 0) {
+//     console.log("Breed not found. Please enter another breed.");
+//   } else {
+//     console.log(data[0]["description"]);
+//   }
+// });
+
 
 // console.log("response: ", response && response.statusCode);
 // shortcut of if/else statment .... && ...
